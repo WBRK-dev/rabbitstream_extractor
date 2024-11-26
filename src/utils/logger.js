8ac2e35a75225949365ab09b40db83f026b873b0
Koert Weber <koert@mooore.nl>
2024-11-26 10:50:14 +0100

    feat: added warn, irrelevant and basic log

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,21 +1,26 @@
-function log(payload, type) {
+function handle(payload, type) {
     if (typeof payload === 'object') {
         payload = JSON.stringify(payload, null, 2);
     } else if (typeof payload !== 'string') {
         return error('Logger payload must be a string or an object');
     }
 
-    let message = '';
+    let message = payload;
 
     switch (type) {
         case "error":
             message = `\x1b[41m\n\n  ${payload}\n\x1b[0m\n`;
             break;
+        case "warn":
+            message = `\x1b[103m\n\n  ${payload}\n\x1b[0m\n`;
+            break;
         case "success":
             message = `\x1b[42m\n\n  ${payload}\n\x1b[0m\n`;
             break;
+        case "irrelevant":
+            message = `\x1b[90m${payload}\x1b[0m`;
+            break;
         case "info":
-        default:
             message = `\x1b[44m\n\n  ${payload}\n\x1b[0m\n`;
             break;
     }
@@ -24,13 +29,25 @@ function log(payload, type) {
 }
 
 export function info(payload) {
-    log(payload, 'info');
+    handle(payload, 'info');
 }
 
 export function error(payload) {
-    log(payload, 'error');
+    handle(payload, 'error');
+}
+
+export function warn(payload) {
+    handle(payload, 'warn');
 }
 
 export function success(payload) {
-    log(payload, 'success');
+    handle(payload, 'success');
+}
+
+export function irrelevant(payload) {
+    handle(payload, 'irrelevant');
+}
+
+export function log(payload) {
+    handle(payload, 'log');
 }
